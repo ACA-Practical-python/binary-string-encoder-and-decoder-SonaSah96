56ccb1318f4dcf20409b46e142c5be377de051a8
Sonasah96 <SonaSah1919@gmail.com>
2021-01-17 14:00:45 +0400

    Task2 changes

--- Task2_encode_decode.py
+++ Task2_encode_decode.py
@@ -1,7 +1,6 @@
 # problem1
 # 1st version
-def en_code():
-    word = input("Please input a string: ")
+def en_code(word):
     bin_word = []
     for item in word:
         bin_word.append(bin(ord(item))[2:])
@@ -9,16 +8,10 @@ def en_code():
     return result
 
 
-print(en_code())
-
-
 # 2nd version
-def en_code1():
-    word = input("Please input a string: ")
+def en_code1(word):
     return "".join([bin(ord(item))[2:] for item in word])
 
-
-print(en_code1())
 # problem 2
 # 1st version
 
@@ -30,13 +23,9 @@ def de_code_input(bin_type):
     return True
 
 
-def de_code():
+def de_code(bin_type):
     stack_num = []
     word = []
-    while True:
-        bin_type = input("Please input a binary code(it can contain only 0 or 1): ")
-        if de_code_input(bin_type) == True:
-            break
     new_bin_type = [item for item in bin_type]
     while len(new_bin_type) > 0:
         while len(stack_num) < 8:
@@ -45,17 +34,9 @@ def de_code():
         stack_num.clear()
     return "".join(word)
 
-
-print(de_code())
 # 2nd version
 
 
-def de_code2():
-    while True:
-        bin_type = input("Please input a binary code(it can contain only 0 or 1): ")
-        if de_code_input(bin_type) == True:
-            break
+def de_code2(bin_type):
     return "".join([chr(int(item1, 2))for item1 in [bin_type[item:item+8] for item in range(0, len(bin_type), 8)]])
 
-
-print(de_code2())
